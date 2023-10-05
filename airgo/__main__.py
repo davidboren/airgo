d82ff24ef42f9e42769e36ffc0a3617a0631e798
David Boren <davidboren@davids-mbp.mynetworksettings.com>
2023-10-05 12:09:50 -0700

    Fix some templates

--- airgo/__main__.py
+++ airgo/__main__.py
@@ -62,7 +62,7 @@ def submit_workflow(workflow_data, parameters):
     with open(tf.name, "w") as temp:
         yaml.dump(
             yaml.load(
-                workflow_data.replace("${Image_Tag}", f"sha-{get_commit_sha()}"),
+                workflow_data.replace("${ImageTag}", f"sha-{get_commit_sha()}"),
                 Loader=yaml.FullLoader,
             ),
             temp,
@@ -200,7 +200,7 @@ def init(
             ) as f:
                 f.write(
                     default_container.replace(
-                        "${SET IMAGE HERE}", f"{docker_repo}:${{Image_Tag}}"
+                        "${SET IMAGE HERE}", f"{docker_repo}:${{ImageTag}}"
                     )
                 )
         copy_template(
@@ -235,7 +235,7 @@ def init(
         ) as f:
             if docker_repo:
                 default_task = default_task.replace(
-                    "${SET IMAGE HERE}", f"{docker_repo}:${{Image_Tag}}"
+                    "${SET IMAGE HERE}", f"{docker_repo}:${{ImageTag}}"
                 )
 
             f.write(default_task)
